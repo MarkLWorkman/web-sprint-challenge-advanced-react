2791f4ad41282467c532afcde6177f82d8a95cc0
unknown <calanhaga@aim.com>
2021-03-19 21:15:24 -0400

    added state and componentDidMount to PlantList

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,17 +3,34 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((response) => {
+        this.setState({
+          plants: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        console.log(res);
-      });
+    axios.get("http://localhost:3333/plants").then((res) => {
+      console.log(res);
+    });
   }
 
   render() {
